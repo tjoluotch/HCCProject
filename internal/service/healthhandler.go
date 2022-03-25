faf5dddcb593786b2daab9e42f9ad54c01add022
tjoluotch <trevorjoluotch@gmail.com>
2022-03-25 07:01:25 +0000

    implemented health handler for service

--- internal/service/healthhandler.go
+++ internal/service/healthhandler.go
@@ -1,11 +1,24 @@
 package service
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type healthResponse struct {
+	Health string `json:"health"`
+}
 
 // HealthHandler is responsible for checking the health of this service.
 // Accessed through /{version}/health
 func (s *Service) HealthHandler() http.HandlerFunc {
-	return func(resp http.ResponseWriter, req *http.Request) {
+	responseBody := &healthResponse{Health: "healthy"}
 
+	return func(resp http.ResponseWriter, req *http.Request) {
+		if err := json.NewEncoder(resp).Encode(responseBody); err != nil {
+			log.Println(err)
+			resp.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
