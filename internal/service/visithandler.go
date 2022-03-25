4df42ff7509bb9a2a42c5d3d8bcf4ef0b28512a5
tjoluotch <trevorjoluotch@gmail.com>
2022-03-25 08:09:05 +0000

    implemented bathroom visit handler

--- internal/service/visithandler.go
+++ internal/service/visithandler.go
@@ -1,12 +1,44 @@
 package service
 
-import "net/http"
+import (
+	"HCCProject/internal/filehandler"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// visitResponse contains the json data for a successful responses regarding client visualisation data points
+type visitResponse struct {
+	Data []filehandler.Data `json:"data"`
+}
+
+// errorResponse contains error if we failed to calculate trend data
+type errorResponse struct {
+	Error string `json:"error"`
+}
 
 // VisitsHandler is responsible processing the bathroom visits file and returning bathroom visits data to the client.
 // Accessed through /{version}/visits
 func (s *Service) VisitsHandler() http.HandlerFunc {
+	visitData, err := s.fh.CalculateTrendDataPoints()
+	var dataPoints []filehandler.Data
+	response := visitResponse{}
 
 	return func(resp http.ResponseWriter, req *http.Request) {
+		// if we failed to calculate trend data return 500 response and errorResponse type
+		if err != nil {
+			log.Println(err)
+			resp.WriteHeader(http.StatusInternalServerError)
+			_ = json.NewEncoder(resp).Encode(&errorResponse{Error: err.Error()})
+		}
 
+		// populate data for response object
+		for _, data := range visitData {
+			dataPoints = append(dataPoints, *data)
+		}
+		// assign the slice to response object
+		response.Data = dataPoints
+
+		_ = json.NewEncoder(resp).Encode(&response)
 	}
 }
